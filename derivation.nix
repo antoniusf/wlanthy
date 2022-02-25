1fbcb9fa2d9ba42799f522b7251282d875f582c2
Antonius Frie <antonius.frie@ruhr-uni-bochum.de>
2022-02-25 15:19:59 +0100

    add nix build files

---
+++ derivation.nix
@@ -0,0 +1,12 @@
+{ lib, stdenv, cmake, meson, ninja, pkg-config, anthy, wayland, wayland-protocols, libxkbcommon }:
+
+stdenv.mkDerivation {
+  name = "wlanthy-oyayubi";
+  src = builtins.fetchGit {
+    url = "git@github.com:antoniusf/wlanthy.git";
+    ref = "thumb-shift";
+    rev = "c7d749a2725092be8350cf23433d47d95c03a548";
+  };
+
+  buildInputs = [ meson ninja cmake pkg-config anthy wayland wayland-protocols libxkbcommon ];
+}
